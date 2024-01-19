7546aa7fe813980913eb77497f8bfea257c05747
Xueliovo <xlywkk@163.com>
2024-01-19 14:40:19 +0800

    Add README File

---
+++ Jiami/CMakeFiles/Jiami.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jiami.dir/main.cpp.o"
+  "CMakeFiles/Jiami.dir/main.cpp.o.d"
+  "Jiami"
+  "Jiami.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jiami.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

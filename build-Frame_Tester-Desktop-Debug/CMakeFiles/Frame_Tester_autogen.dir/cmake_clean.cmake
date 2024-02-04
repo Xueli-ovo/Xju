c7de4ae7ef929dd7d18666d0a7bde7970efbd3b9
Xueliovo <xlywkk@163.com>
2024-02-04 16:42:22 +0800

    创建Qt工程项目

---
+++ build-Frame_Tester-Desktop-Debug/CMakeFiles/Frame_Tester_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Frame_Tester_autogen"
+  "Frame_Tester_autogen/include/ui_mainwindow.h"
+  "Frame_Tester_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Frame_Tester_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

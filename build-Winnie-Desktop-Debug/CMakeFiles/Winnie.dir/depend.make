e32b45ee0ebc01ae57d9c26fe2ea5bbc80f3fb0c
Xueliovo <xlywkk@163.com>
2024-04-06 12:48:26 +0800

    删除：     Frame/CMakeLists.txt.user 	删除：     Frame/mainwindow.ui 	新文件：   Winnie/.gitignore 	重命名：   Frame/CMakeLists.txt -> Winnie/CMakeLists.txt 	重命名：   Frame/main.cpp -> Winnie/main.cpp

---
+++ build-Winnie-Desktop-Debug/CMakeFiles/Winnie.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Winnie.
+# This may be replaced when dependencies are built.
--- hpCardGame/lib.cpp
+++ hpCardGame/lib.cpp
@@ -6,7 +6,7 @@
 
 
 #include <bits/stdc++.h>
-#include <string.h>
+#include <ncurses.h>
 
 
 class info {
@@ -27,6 +27,13 @@ public:
 class libtalk {
 public:
     std::string welcome = "欢迎来到hpCardGame!\n输入\"help new\"获取新手帮助.";
+    int main(std::string com){
+	    //此方法用于处理用户输入的命令.
+    }
+
+
+
+protected:
 
     void GetHelp(std::string status) {
         std::string ver = "0.0.1";
@@ -42,8 +49,34 @@ public:
             std::cout << "游戏开始时,会先指定玩家名,您需要输入您和对手的名字以开始游戏.\n";
             std::cout << "在游戏中,双方会抽取手牌,您要利用手上的牌来击败对手\n";
             std::cout << "同时,双方出牌会受到体力的限制,使得您要慎重思考如何出牌.\n\n";
+	    std::cout << "下面，请您观看一段演示\n";
         } else {
             std::cout << "参数不正确,尝试运行help ?试试.\n";
         }
     }
 };
+
+class player {
+	public:
+		std::string name;//玩家名字
+		std::string chara;
+
+		int HP;//血量
+		int MP;
+		int AP;
+		int DP;
+}
+
+
+
+
+int shell(){
+	libtalk one;
+	std::cout<<"hpCardGame Shell\n";
+	std::cout<<one.welcome<<endl;
+
+	while (true){
+		std::cout<<"shell >";
+		std::cin>>one.main;
+	}
+
